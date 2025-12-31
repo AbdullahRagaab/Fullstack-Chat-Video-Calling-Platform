cc14e0060f67d241244bfd250dc3c3f99301f3b7
Abdullah Ragab <abdullahragab460@gmail.com>
2025-12-31 17:46:28 +0200

    Fix auth cookies for cross-domain

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -53,8 +53,10 @@ export async function signup(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      // sameSite: "strict", // prevent CSRF attacks
+      // secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      secure: true,
     });
 
     res.status(201).json({ success: true, user: newUser });
@@ -85,8 +87,10 @@ export async function login(req, res) {
     res.cookie("jwt", token, {
       maxAge: 7 * 24 * 60 * 60 * 1000,
       httpOnly: true, // prevent XSS attacks,
-      sameSite: "strict", // prevent CSRF attacks
-      secure: process.env.NODE_ENV === "production",
+      // sameSite: "strict", // prevent CSRF attacks
+      // secure: process.env.NODE_ENV === "production",
+      sameSite: "none",
+      secure: true,
     });
 
     res.status(200).json({ success: true, user });
