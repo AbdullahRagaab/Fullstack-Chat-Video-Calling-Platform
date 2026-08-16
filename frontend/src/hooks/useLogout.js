da9d60c98366c862c3971cbb248fb9c2f45663f9
Abdullah Ragab <118401604+AbdullahRagaab@users.noreply.github.com>
2026-08-16 23:37:51 +0300

    Modify useLogout to clear authUser on logout
    Update onSuccess handler to set authUser data to null.

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,3 +1,24 @@
+// import { useMutation, useQueryClient } from "@tanstack/react-query";
+// import { logout } from "../lib/api";
+
+// const useLogout = () => {
+//   const queryClient = useQueryClient();
+
+//   const {
+//     mutate: logoutMutation,
+//     isPending,
+//     error,
+//   } = useMutation({
+//     mutationFn: logout,
+//     onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+//   });
+
+//   return { logoutMutation, isPending, error };
+// };
+// export default useLogout;
+
+
+
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { logout } from "../lib/api";
 
@@ -10,9 +31,12 @@ const useLogout = () => {
     error,
   } = useMutation({
     mutationFn: logout,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+    onSuccess: () => {
+      queryClient.setQueryData(["authUser"], null);
+    },
   });
 
   return { logoutMutation, isPending, error };
 };
+
 export default useLogout;
